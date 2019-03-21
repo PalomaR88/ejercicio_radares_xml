8aa98735656f5d67a74dda0fce3cfa83e9176abe
Paloma R. García <palomagarciacampon08@gmail.com>
2019-03-21 10:16:32 +0100

    empiezo .py

---
+++ inforadares.py
@@ -0,0 +1,79 @@
+#importar xml
+from lxml import etree
+doc=etree.parse('radares.xml')
+
+#función para el ejercicio 1
+def ejercicio1(doc, ruta):
+    lista_repetida=datos(doc, ruta)
+    lista_sin_repetir=datosSinRepetir(lista_repetida)
+    return lista_sin_repetir
+
+#función que crea una lista a partir de una ruta
+def datos(doc, ruta):
+    lista=[]
+    dato=doc.xpath(ruta)
+    for i in dato:
+        lista.append(i)
+    return lista
+
+#función que recibe una lista y elimina los valores repetidos
+def datosSinRepetir(lista):
+    lista_sin_repetir=[]
+    for i in lista:
+        existe=False
+        for x in len(lista):
+#            if lista[i]==lista_sin_repetir[x]:
+#                existe=True
+#        if existe==False:
+#            lista_sin_repetir.append(lista[i])
+            print(x)
+    return lista_sin_repetir
+
+
+
+#menu principal
+while True:
+    print('''
+1.- Listar las provincias de las que tenemos información sobre radares
+2.- Contar los radares de los que tenemos información
+3.- Nombre de las carreteras y cantidad de radares según provincia
+4.- Mostrar provincias y radares según carretera
+5.- Mostrar radares y coordenadas de estos según carretera
+0.- Salir''')
+    opcion=input("Opción: ")
+    print("")
+    if opcion=="1":
+        for i in range(len(ejercicio1(doc,"/RAIZ/PROVINCIA/NOMBRE/text()"))):
+            print(i, "-", ejercicio1(doc,"/RAIZ/PROVINCIA/NOMBRE/text()")[i])
+        print("")
+        tecla= input("PRESIONA UNA INTRO PARA CONTINUAR")
+
+    elif opcion=="2":
+
+        print(" ")
+        tecla= input("PRESIONA UNA INTRO PARA CONTINUAR")
+
+    elif opcion=="3":
+        print("")
+        tecla= input("PRESIONA UNA INTRO PARA CONTINUAR")
+
+    elif opcion=="4":
+
+        print("")
+        tecla= input("PRESIONA UNA INTRO PARA CONTINUAR")
+
+    elif opcion=="5":
+
+        print("")
+        tecla= input("PRESIONA UNA INTRO PARA CONTINUAR")
+
+    elif opcion=="0":
+        print("Adios")
+        break
+    else:
+        print("Lo siento, no hay ninguna opción disponible")
+        print(" ")
+             
+
+
+
